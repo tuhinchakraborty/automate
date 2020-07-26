ad1079500436f1ac07e7182e3cc6a04d8f06d149
tuhinchakraborty <tuhin_chakraborty@outlook.com>
2020-07-26 17:02:31 +0530

    Initial Commit

---
+++ scripts/parse_wikipedia.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+import schedule
+import requests
+from bs4 import BeautifulSoup
+
+load_dotenv()
+
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+from_whatsapp_number = os.environ['TWILIO_NUMBER']
+to_whatsapp_number = os.environ['WHATSAPP_RECEIVER_NUMBER']
+WIKIPEDIA_BASE_URL = os.environ['WIKIPEDIA_BASE_URL']
+WIKIPEDIA_MAIN_PAGE = os.environ['WIKIPEDIA_MAIN_PAGE']
+
+
+def get_featured_article():
+    req = requests.get(WIKIPEDIA_BASE_URL + WIKIPEDIA_MAIN_PAGE)
+    soup = BeautifulSoup(req.content, "html.parser")
+    span = soup.find(id="From_today's_featured_article")
+    feature_article_header = span.parent
+    feature_article_body = feature_article_header.findNext('div')
+    feature_article_relative_path = feature_article_body \
+        .findNext('p') \
+        .findNext('b') \
+        .find('a') \
+        .get('href')
+    return feature_article_relative_path
+
+
+def send_message():
+    message = """From today's featured article\n""" + WIKIPEDIA_BASE_URL + get_featured_article()
+    client.messages.create(body=message, from_=from_whatsapp_number, to=to_whatsapp_number)
+
+
+schedule.every(1).seconds.do(send_message)
+
+while True:
+    schedule.run_pending()
